47091750175c2375692c3ec243bc38b32877a36f
Lyd1aCla1r3 <l.pedersen@entando.com>
2022-01-06 18:25:02 -0800

    CI-CD add directory

---
+++ ui/widgets/conference/formWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,8 @@
+const conferenceMock = {
+  name:
+    'Et sed quia aut. Quia iste praesentium impedit ipsum modi iusto non eligendi illo. Molestias possimus similique quod vel. Veritatis eius beatae voluptatem et excepturi debitis libero. Reprehenderit saepe vel debitis dignissimos itaque porro.',
+  location:
+    'Corrupti iste fugiat qui totam excepturi ut non iure. Ex nihil officiis odio deserunt ex. Deserunt corrupti sunt ea id cum officiis. Rerum rem dolorem reprehenderit eum odio.',
+};
+
+export default conferenceMock;
---
+++ ui/widgets/conference/tableWidget/src/components/__mocks__/conferenceMocks.js
@@ -0,0 +1,18 @@
+const conferenceMocks = [
+  {
+    id: 0,
+    name:
+      'Et sed quia aut. Quia iste praesentium impedit ipsum modi iusto non eligendi illo. Molestias possimus similique quod vel. Veritatis eius beatae voluptatem et excepturi debitis libero. Reprehenderit saepe vel debitis dignissimos itaque porro.',
+    location:
+      'Corrupti iste fugiat qui totam excepturi ut non iure. Ex nihil officiis odio deserunt ex. Deserunt corrupti sunt ea id cum officiis. Rerum rem dolorem reprehenderit eum odio.',
+  },
+  {
+    id: 1,
+    name:
+      'Vel sed ullam debitis occaecati ut et dolorem odit. Rerum voluptatem autem ut. Adipisci et libero est. Quam veritatis culpa doloribus.',
+    location:
+      'Delectus laborum dolor ex beatae ut nostrum. Provident nam molestiae amet aut. Dolore explicabo praesentium quis repudiandae facilis. Velit ipsa quae dolorem et. Ea iure ut sed est quasi unde. Ut facere voluptates et.',
+  },
+];
+
+export default conferenceMocks;
